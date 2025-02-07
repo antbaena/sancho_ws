638b6f068d95db6407bad687d13fd90920def53a
Antonio Cañete Baena <antoniocanetebaena1234@gmail.com>
2025-02-07 13:22:20 +0100

    added skeleton launch

---
+++ src/sancho_perception/launch/skeleton_nodes_launch.py
@@ -0,0 +1,27 @@
+
+#!/usr/bin/env python3
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # Nodo para la detección de pose 2D y publicación de imagen anotada
+    skeleton_detection_node = Node(
+        package='sancho_perception',
+        executable='skeleton_detection_node',
+        name='skeleton_detection_node',
+        output='screen'
+    )
+
+    # Nodo para la transformación de keypoints 2D a 3D y publicación de imagen de profundidad anotada
+    skeleton_3d_node = Node(
+        package='sancho_perception',
+        executable='skeleton_3d_node',
+        name='skeleton_3d_node',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        skeleton_detection_node,
+        skeleton_3d_node
+    ])
--- src/sancho_perception/setup.py
+++ src/sancho_perception/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
-
+import os
+from glob import glob
 package_name = 'sancho_perception'
 
 setup(
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", package_name, "launch"), glob("launch/*")),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'skeleton_detector = sancho_perception.skeleton_detector:main',
+            'skeleton_detection_node = sancho_perception.skeleton_detector:main',
             'skeleton_3d_node = sancho_perception.skeleton_3d_node:main',
         ],
     },
