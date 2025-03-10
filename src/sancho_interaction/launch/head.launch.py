9a35b68fa39b70b73b4623bcf474bcfe31c0ad53
Antonio Cañete Baena <antoniocanetebaena1234@gmail.com>
2025-03-10 15:13:50 +0100

    remove bad interbotix pkgs

--- src/sancho_interaction/launch/head.launch.py
+++ src/sancho_interaction/launch/head.launch.py
@@ -1,7 +1,9 @@
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource, AnyLaunchDescriptionSource
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     return LaunchDescription([
