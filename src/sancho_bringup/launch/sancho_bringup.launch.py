5d432321d648848ca624207fe03efba3fca9854f
Antonio Cañete Baena <antoniocanetebaena1234@gmail.com>
2025-03-27 11:24:14 +0100

    updated ñaunch files

--- src/sancho_bringup/launch/sancho_bringup.launch.py
+++ src/sancho_bringup/launch/sancho_bringup.launch.py
@@ -1,8 +1,9 @@
 import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource, AnyLaunchDescriptionSource
-from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, GroupAction
+from launch_ros.actions import Node, PushRosNamespace
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
@@ -15,69 +16,71 @@ def generate_launch_description():
         'sancho_ranger.urdf'
     )
 
-    # Lanzar LIDAR (ejemplo con Hokuyo)
+    # Lanzar LiDAR Hokuyo
     hokuyo_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('urg_node2'), 'launch'),
-            '/urg_node2_2lidar.launch.py'])
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('urg_node2'), 'launch', 'urg_node2_2lidar.launch.py')
+        )
     )
 
-    # Lanzar cámara Orbbec Gemini 330
-    orbbec_camera_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('orbbec_camera'), 'launch'),
-            '/gemini_330_series.launch.py'])
-    )
+    # Lanzar cámara Orbbec Gemini 330 en namespace independiente
+    orbbec_camera_launch = GroupAction([
+        PushRosNamespace('gemini_camera'),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('orbbec_camera'), 'launch', 'gemini_330_series.launch.py')
+            ),
+            launch_arguments={
+                'camera_name': 'gemini_cam',
+                'serial_number': 'AARX3410080',
+                'usb_port': '2-1',
+                'device_num': '1',
+                'sync_mode': 'standalone'
+            }.items()
+        )
+    ])
+
+    # Lanzar cámara Astra en namespace independiente
+    astra_camera_launch = GroupAction([
+        PushRosNamespace('astra_camera'),
+        IncludeLaunchDescription(
+            XMLLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('astra_camera'), 'launch', 'astra.launch.xml')
+            ),
+            launch_arguments={
+                'serial_number': "'20070830098'",
+                'camera_name': 'camera',
+                'color_fps': '15',
+                'depth_fps': '15',
+                'ir_fps': '15'
+
+                
+            }.items()
+        )
+    ])
 
     return LaunchDescription([
-        # Publicar estado del robot desde URDF
+        # robot_state_publisher (URDF)
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            name='robot_state_publisher',
             output='screen',
             parameters=[{'robot_description': open(urdf_file_path).read()}]
         ),
 
-        # Publicar estado de articulaciones si es necesario
+        # joint_state_publisher
         Node(
             package='joint_state_publisher',
             executable='joint_state_publisher',
-            name='joint_state_publisher',
             output='screen'
         ),
 
-        # Cámara Astra
-        # Node(
-        #      package='astra_camera',
-        #      executable='astra_camera_node',
-        #      name='astra_camera_node',
-        #      output='screen'
-        #  ),
-
-        # Cámara USB
-        # Node(
-        #     package="usb_cam",
-        #     executable="usb_cam_node_exe",
-        #     name="usb_cam_node",
-        #     output="screen",
-        #     prefix="xterm -hold -e",
-        #     emulate_tty=True,
-        #     parameters=[
-        #         os.path.join(
-        #             get_package_share_directory("sancho_bringup"),
-        #             "config",
-        #             "params_low.yaml",
-        #         )
-        #     ],
-        # ),
-
-        # Incluir el launch de la base móvil
+        # Launch de la base móvil Ranger
         IncludeLaunchDescription(
-            AnyLaunchDescriptionSource(os.path.join(ranger_bringup_path, 'launch', 'ranger_mini_v2.launch.xml'))
+            XMLLaunchDescriptionSource(os.path.join(ranger_bringup_path, 'launch', 'ranger_mini_v2.launch.xml'))
         ),
 
-        # Iniciar sensores (LiDAR, cámaras, etc.)
+        # Lanzamiento de sensores
         hokuyo_launch,
-        # orbbec_camera_launch,
+        astra_camera_launch
     ])
