1fe1aa576b34257d020f38a20c609fafe2056480
Antonio Cañete Baena <70489950+antbaena@users.noreply.github.com>
2025-06-14 22:50:51 +0200

    Add strategy pattern for TDOA

--- src/sancho_audio/sancho_audio/tdoa_nodes/tdoa_gcc.py
+++ src/sancho_audio/sancho_audio/tdoa_nodes/tdoa_gcc.py
@@ -1,11 +1,14 @@
 import numpy as np
 
-def gcc_phat(signal_1: np.ndarray,
-             signal_2: np.ndarray,
-             fs: int,
-             max_tau: float = None,
-             interp: int = 16):
-    '''Estimate time-delay using GCC-PHAT'''  
+
+def gcc_phat(
+    signal_1: np.ndarray,
+    signal_2: np.ndarray,
+    fs: int,
+    max_tau: float = None,
+    interp: int = 16,
+):
+    """Estimate time-delay using GCC-PHAT"""
     n = signal_1.shape[0] + signal_2.shape[0]
     # FFT of both signals
     SIG1 = np.fft.rfft(signal_1, n=n)
@@ -20,7 +23,7 @@ def gcc_phat(signal_1: np.ndarray,
     max_shift = int(interp * n / 2)
     if max_tau:
         max_shift = np.minimum(int(interp * fs * max_tau), max_shift)
-    cc = np.concatenate((cc[-max_shift:], cc[:max_shift+1]))
+    cc = np.concatenate((cc[-max_shift:], cc[: max_shift + 1]))
     # Find max index
     shift = np.argmax(np.abs(cc)) - max_shift
     tau = shift / float(interp * fs)
